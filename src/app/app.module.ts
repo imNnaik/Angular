9d0d1a4e372f69629708335172eb71cbbbb139dd
imNnaik <naiknaveen93@gmail.com>
2021-01-02 17:52:14 +0530

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatButtonModule,FlexLayoutModule],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, MatButtonModule,FlexLayoutModule],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
